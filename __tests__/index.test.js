52f23c1be40c475f2bc806758d38b3e35e04ab0a
Gustavo Robles <gustavo@cl.ly>
2020-04-15 18:04:12 -0500

    Update from ruby 2.5.0 to 2.5.7

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,132 +1,137 @@
-const Plugin = require("../index");
+const Plugin = require('../index');
 
 const process = require('process');
-
 
 let plugin = null;
 let serverless = null;
 let options = null;
 
-beforeEach(()=>{
+beforeEach(() => {
   serverless = {
-    cli: {log: () => {}},
+    cli: { log: () => {} },
     config: {
-      servicePath: process.cwd() + "/__tests__/demo_service"
+      servicePath: process.cwd() + '/__tests__/demo_service',
     },
     service: {
       package: {
-        include: ["handler.rb", "lib/**"],
-        exclude: ["mytest.rb"]
+        include: ['handler.rb', 'lib/**'],
+        exclude: ['mytest.rb'],
       },
       provider: {
-        name: "aws",
-        runtime: "ruby2.5"
+        name: 'aws',
+        runtime: 'ruby2.5',
       },
       custom: {
         rubyPackage: {
           alwaysCrossCompileExtensions: false,
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  };
   options = {};
 
   plugin = new Plugin(serverless, options);
 });
 
-test("captures the serverless configuration", () => {
+test('captures the serverless configuration', () => {
   expect(plugin.serverless).toBe(serverless);
 });
 
-test("captures the options", () => {
+test('captures the options', () => {
   expect(plugin.options).toBe(options);
 });
 
-test("hooks in before packaging deployment artifacts", () => {
-  expect(Object.keys(plugin.hooks)).toContain("before:package:createDeploymentArtifacts");
+test('hooks in before packaging deployment artifacts', () => {
+  expect(Object.keys(plugin.hooks)).toContain(
+    'before:package:createDeploymentArtifacts'
+  );
 });
 
-test("hooks in before packaging an individual function", () => {
-  expect(Object.keys(plugin.hooks)).toContain("before:package:function:package");
+test('hooks in before packaging an individual function', () => {
+  expect(Object.keys(plugin.hooks)).toContain(
+    'before:package:function:package'
+  );
 });
 
-test("disables 'Excluding development dependencies', which only applies to node projects", () =>{
+test("disables 'Excluding development dependencies', which only applies to node projects", () => {
   plugin.beforePackage();
   expect(plugin.serverless.service.package.excludeDevDependencies).toBe(false);
 });
 
-test("forces whitelisting files to package by excluding all files by default", () =>{
+test('forces whitelisting files to package by excluding all files by default', () => {
   plugin.beforePackage();
-  expect(plugin.serverless.service.package.exclude).toEqual(["**"]);
+  expect(plugin.serverless.service.package.exclude).toEqual(['**']);
 });
 
-test("include the bundler standalone files", () => {
+test('include the bundler standalone files', () => {
   plugin.beforePackage();
 
-  expect(plugin.serverless.service.package.include).toContain("vendor/bundle/bundler/**");
+  expect(plugin.serverless.service.package.include).toContain(
+    'vendor/bundle/bundler/**'
+  );
 });
 
-test("preserve the includes specified in serverless configuration", () => {
+test('preserve the includes specified in serverless configuration', () => {
   plugin.beforePackage();
 
-  expect(plugin.serverless.service.package.include).toContain("handler.rb");
-  expect(plugin.serverless.service.package.include).toContain("lib/**");
+  expect(plugin.serverless.service.package.include).toContain('handler.rb');
+  expect(plugin.serverless.service.package.include).toContain('lib/**');
 });
 
-test("include files for each gem needed by default bundler group - excluding .git/test files", () =>{
+test('include files for each gem needed by default bundler group - excluding .git/test files', () => {
   plugin.beforePackage();
 
   // asserting on entire array since order matters
   expect(plugin.serverless.service.package.include).toEqual([
-    "handler.rb",
-    "lib/**",
-    "vendor/bundle/bundler/**",
+    'handler.rb',
+    'lib/**',
+    'vendor/bundle/bundler/**',
 
-    "vendor/bundle/ruby/2.5.0/gems/addressable-2.6.0/**",
-    "!vendor/bundle/ruby/2.5.0/gems/addressable-2.6.0/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/addressable-2.6.0/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/addressable-2.6.0/spec/**",
-    "vendor/bundle/ruby/2.5.0/gems/domain_name-0.5.20180417/**",
-    "!vendor/bundle/ruby/2.5.0/gems/domain_name-0.5.20180417/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/domain_name-0.5.20180417/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/domain_name-0.5.20180417/spec/**",
-    "vendor/bundle/ruby/2.5.0/gems/http-4.1.1/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http-4.1.1/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http-4.1.1/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http-4.1.1/spec/**",
-    "vendor/bundle/ruby/2.5.0/gems/http-cookie-1.0.3/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http-cookie-1.0.3/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http-cookie-1.0.3/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http-cookie-1.0.3/spec/**",
-    "vendor/bundle/ruby/2.5.0/gems/http-form_data-2.1.1/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http-form_data-2.1.1/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http-form_data-2.1.1/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http-form_data-2.1.1/spec/**",
-    "vendor/bundle/ruby/2.5.0/gems/http_parser.rb-0.6.0/**",
-    "vendor/bundle/ruby/2.5.0/extensions/x86_64-linux/2.5.0-static/http_parser.rb-0.6.0/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http_parser.rb-0.6.0/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http_parser.rb-0.6.0/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/http_parser.rb-0.6.0/spec/**",
-    "vendor/bundle/ruby/2.5.0/gems/public_suffix-3.0.3/**",
-    "!vendor/bundle/ruby/2.5.0/gems/public_suffix-3.0.3/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/public_suffix-3.0.3/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/public_suffix-3.0.3/spec/**",
-    "vendor/bundle/ruby/2.5.0/gems/redis-4.1.0/**",
-    "!vendor/bundle/ruby/2.5.0/gems/redis-4.1.0/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/redis-4.1.0/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/redis-4.1.0/spec/**",
-    "vendor/bundle/ruby/2.5.0/gems/redis-namespace-1.6.0/**",
-    "!vendor/bundle/ruby/2.5.0/gems/redis-namespace-1.6.0/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/redis-namespace-1.6.0/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/redis-namespace-1.6.0/spec/**",
-    "vendor/bundle/ruby/2.5.0/gems/unf-0.1.4/**",
-    "!vendor/bundle/ruby/2.5.0/gems/unf-0.1.4/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/unf-0.1.4/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/unf-0.1.4/spec/**",
-    "vendor/bundle/ruby/2.5.0/gems/unf_ext-0.0.7.5/**",
-    "vendor/bundle/ruby/2.5.0/extensions/x86_64-linux/2.5.0-static/unf_ext-0.0.7.5/**",
-    "!vendor/bundle/ruby/2.5.0/gems/unf_ext-0.0.7.5/.git/**",
-    "!vendor/bundle/ruby/2.5.0/gems/unf_ext-0.0.7.5/test/**",
-    "!vendor/bundle/ruby/2.5.0/gems/unf_ext-0.0.7.5/spec/**",
+    'vendor/bundle/ruby/2.5.7/gems/addressable-2.6.0/**',
+    '!vendor/bundle/ruby/2.5.7/gems/addressable-2.6.0/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/addressable-2.6.0/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/addressable-2.6.0/spec/**',
+    'vendor/bundle/ruby/2.5.7/gems/domain_name-0.5.20180417/**',
+    '!vendor/bundle/ruby/2.5.7/gems/domain_name-0.5.20180417/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/domain_name-0.5.20180417/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/domain_name-0.5.20180417/spec/**',
+    'vendor/bundle/ruby/2.5.7/gems/http-4.1.1/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http-4.1.1/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http-4.1.1/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http-4.1.1/spec/**',
+    'vendor/bundle/ruby/2.5.7/gems/http-cookie-1.0.3/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http-cookie-1.0.3/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http-cookie-1.0.3/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http-cookie-1.0.3/spec/**',
+    'vendor/bundle/ruby/2.5.7/gems/http-form_data-2.1.1/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http-form_data-2.1.1/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http-form_data-2.1.1/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http-form_data-2.1.1/spec/**',
+    'vendor/bundle/ruby/2.5.7/gems/http_parser.rb-0.6.0/**',
+    'vendor/bundle/ruby/2.5.7/extensions/x86_64-linux/2.5.7-static/http_parser.rb-0.6.0/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http_parser.rb-0.6.0/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http_parser.rb-0.6.0/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/http_parser.rb-0.6.0/spec/**',
+    'vendor/bundle/ruby/2.5.7/gems/public_suffix-3.0.3/**',
+    '!vendor/bundle/ruby/2.5.7/gems/public_suffix-3.0.3/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/public_suffix-3.0.3/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/public_suffix-3.0.3/spec/**',
+    'vendor/bundle/ruby/2.5.7/gems/redis-4.1.0/**',
+    '!vendor/bundle/ruby/2.5.7/gems/redis-4.1.0/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/redis-4.1.0/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/redis-4.1.0/spec/**',
+    'vendor/bundle/ruby/2.5.7/gems/redis-namespace-1.6.0/**',
+    '!vendor/bundle/ruby/2.5.7/gems/redis-namespace-1.6.0/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/redis-namespace-1.6.0/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/redis-namespace-1.6.0/spec/**',
+    'vendor/bundle/ruby/2.5.7/gems/unf-0.1.4/**',
+    '!vendor/bundle/ruby/2.5.7/gems/unf-0.1.4/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/unf-0.1.4/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/unf-0.1.4/spec/**',
+    'vendor/bundle/ruby/2.5.7/gems/unf_ext-0.0.7.5/**',
+    'vendor/bundle/ruby/2.5.7/extensions/x86_64-linux/2.5.7-static/unf_ext-0.0.7.5/**',
+    '!vendor/bundle/ruby/2.5.7/gems/unf_ext-0.0.7.5/.git/**',
+    '!vendor/bundle/ruby/2.5.7/gems/unf_ext-0.0.7.5/test/**',
+    '!vendor/bundle/ruby/2.5.7/gems/unf_ext-0.0.7.5/spec/**',
   ]);
 });
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const path = require("path");
+const path = require('path');
 const { execSync } = require('child_process');
 const fs = require('fs');
 
@@ -11,10 +11,9 @@ class PackageRubyBundlePlugin {
         alwaysCrossCompileExtensions: true,
         debug: !!process.env.SRP_DEBUG,
       },
-      (
-        this.serverless.service.custom &&
-        this.serverless.service.custom.rubyPackage
-      ) || {}
+      (this.serverless.service.custom &&
+        this.serverless.service.custom.rubyPackage) ||
+        {}
     );
     return config;
   }
@@ -29,15 +28,15 @@ class PackageRubyBundlePlugin {
     };
   }
 
-  log(message){
-    this.serverless.cli.log(message, "ruby-package");
+  log(message) {
+    this.serverless.cli.log(message, 'ruby-package');
   }
 
-  beforePackage(){
+  beforePackage() {
     this.warnOnUnsupportedRuntime();
 
-    const gemRoot = "vendor/bundle/ruby/2.5.0"; //TODO: infer? configure?
-    const extensionDir = `${gemRoot}/extensions/x86_64-linux/2.5.0-static`;
+    const gemRoot = 'vendor/bundle/ruby/2.5.7'; //TODO: infer? configure?
+    const extensionDir = `${gemRoot}/extensions/x86_64-linux/2.5.7-static`;
 
     const excludeGemTests = true; //TODO: make configurable
     const identifyGemsScript = `
@@ -52,68 +51,95 @@ class PackageRubyBundlePlugin {
         }
       }
       puts JSON.generate(details.sort_by{|x| x[:name]})
-    `
+    `;
 
     this.serverless.service.package.excludeDevDependencies = false; // only relevant to nodejs
-    this.serverless.service.package.exclude = ["**"]; // force whitelist
-    this.serverless.service.package.include.push("vendor/bundle/bundler/**"); // bundler standalone files
+    this.serverless.service.package.exclude = ['**']; // force whitelist
+    this.serverless.service.package.include.push('vendor/bundle/bundler/**'); // bundler standalone files
 
-    const gemFilePath = path.join(this.serverless.config.servicePath, "Gemfile");
-    const bundleEnv = Object.assign({
-      "BUNDLE_GEMFILE": gemFilePath
-    }, process.env);
-    const output = execSync("bundle exec ruby", {input: identifyGemsScript, env: bundleEnv});
-    const gems = JSON.parse(output)
+    const gemFilePath = path.join(
+      this.serverless.config.servicePath,
+      'Gemfile'
+    );
+    const bundleEnv = Object.assign(
+      {
+        BUNDLE_GEMFILE: gemFilePath,
+      },
+      process.env
+    );
+    const output = execSync('bundle exec ruby', {
+      input: identifyGemsScript,
+      env: bundleEnv,
+    });
+    const gems = JSON.parse(output);
 
-    if (gems.some(x=>x.extensions)){
-      // puts Gem.extension_api_version # => 2.5.0-static
-      if (this.config.alwaysCrossCompileExtensions){
+    if (gems.some((x) => x.extensions)) {
+      // puts Gem.extension_api_version # => 2.5.7-static
+      if (this.config.alwaysCrossCompileExtensions) {
         this.nativeLinuxBundle();
       }
     }
 
     if (gems.length < 10) {
-      this.log(`Packaging gems: ${gems.map(x=>x.name).join(" ")}`);
+      this.log(`Packaging gems: ${gems.map((x) => x.name).join(' ')}`);
     } else {
       this.log(`Packaging ${gems.length} gems`);
     }
 
-    gems.forEach((gem) =>{
+    gems.forEach((gem) => {
       this.serverless.service.package.include.push(`${gemRoot}${gem.path}/**`);
-      if (gem.extensions){
-        this.serverless.service.package.include.push(`${extensionDir}/${gem.name}/**`);
+      if (gem.extensions) {
+        this.serverless.service.package.include.push(
+          `${extensionDir}/${gem.name}/**`
+        );
       }
 
       // includes that start with a ! are treated as excludes when evaluating,
       // but are ordered along with the includes. If these patterns were
       // specified as excludes, they would be evaluated first, and then the
       // includes on the gem paths would bring them back.
-      this.serverless.service.package.include.push(`!${gemRoot}${gem.path}/.git/**`);
+      this.serverless.service.package.include.push(
+        `!${gemRoot}${gem.path}/.git/**`
+      );
       if (excludeGemTests) {
-        this.serverless.service.package.include.push(`!${gemRoot}${gem.path}/test/**`);
-        this.serverless.service.package.include.push(`!${gemRoot}${gem.path}/spec/**`);
+        this.serverless.service.package.include.push(
+          `!${gemRoot}${gem.path}/test/**`
+        );
+        this.serverless.service.package.include.push(
+          `!${gemRoot}${gem.path}/spec/**`
+        );
       }
     });
   }
 
-  nativeLinuxBundle(){
+  nativeLinuxBundle() {
     this.log(`Building gems with native extensions for linux`);
     const localPath = this.serverless.config.servicePath;
-    execSync(`docker run --rm -v "${localPath}:/var/task" lambci/lambda:build-ruby2.5 bundle install --standalone --path vendor/bundle`)
+    execSync(
+      `docker run --rm -v "${localPath}:/var/task" lambci/lambda:build-ruby2.5 bundle install --standalone --path vendor/bundle`
+    );
   }
 
-  warnOnUnsupportedRuntime(){
-    if (this.config.debug){
+  warnOnUnsupportedRuntime() {
+    if (this.config.debug) {
       this.log(`platform: ${process.platform}`);
       this.log(`provider: ${this.serverless.service.provider.name}`);
       this.log(`runtime: ${this.serverless.service.provider.runtime}`);
     }
-    if (this.serverless.service.provider.name != 'aws'){
-      this.log(`WARNING: serverless-ruby-package has only been tested with the AWS provider. It may not work with ${this.serverless.service.provider.name}, but bug reports are welcome.`);
+    if (this.serverless.service.provider.name != 'aws') {
+      this.log(
+        `WARNING: serverless-ruby-package has only been tested with the AWS provider. It may not work with ${
+          this.serverless.service.provider.name
+        }, but bug reports are welcome.`
+      );
       return;
     }
-    if (this.serverless.service.provider.runtime != 'ruby2.5'){
-      this.log(`WARNING: serverless-ruby-package has only been tested with the ruby2.5 runtime. It may not work with ${this.serverless.service.provider.runtime}, but bug reports are welcome.`);
+    if (this.serverless.service.provider.runtime != 'ruby2.5') {
+      this.log(
+        `WARNING: serverless-ruby-package has only been tested with the ruby2.5 runtime. It may not work with ${
+          this.serverless.service.provider.runtime
+        }, but bug reports are welcome.`
+      );
     }
   }
 }
